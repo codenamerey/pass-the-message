2c7c7d8b3dda99031740cee2a2a7acbbc72a3806
reysgitcode <127261720+reysgitcode@users.noreply.github.com>
2023-03-20 13:29:22 +0800

    Change answers index page to show all questions

--- src/pages/answers/index.tsx
+++ src/pages/answers/index.tsx
@@ -1,15 +1,6 @@
-import { 
-    Button, 
+import {  
     Box,
-    Text,
-    FormHelperText,
-    FormControl,
-    Tabs,
-    TabList,
-    TabPanels,
-    TabPanel,
-    Tab,
-    Textarea
+    Text
 } from '@chakra-ui/react';
 import { IsPathDefaultUndefined } from 'mongoose/types/inferschematype.js';
 import type {
@@ -25,7 +16,8 @@ import AnswerPanel from '@/components/AnswerPanel';
 
 type Data = {
     question: string,
-    wanted_answers: string[]
+    wanted_answers: string[],
+    slug: string
 }
 
 export interface Answers {
@@ -36,61 +28,30 @@ export interface Answers {
 
 const Answer = ({data}:InferGetServerSidePropsType<typeof getServerSideProps>) => {
 
-    const [answers, setAnswers] = useState<Answers>({
-        quirky: '',
-        serious: '',
-        combined: ''
-    })
-    const isCombinedValid:Boolean | undefined = (!answers.quirky && !answers.serious) 
-    const areSingularsValid:Boolean | undefined = (!answers.combined)
-
-    const {
-        question,
-        wanted_answers
-    } = data;
-
   return (
-    <form>
-        <FormControl>
-            <Box className=' grow flex flex-col justify-center items-center p-4'>
-                <Box className='aspect-square bg-white p-6 flex justify-center items-center rounded-lg'>
-                    <Text>{question}</Text>
-                </Box>
-                <FormHelperText color='white'>Requested Answers: {
-                    !wanted_answers.length ? 'None' :
-                    wanted_answers.length > 1 ? (`${wanted_answers[0]} and ${wanted_answers[1]}`) :
-                    wanted_answers[0]
-                }</FormHelperText>
-
-                <Tabs bg='gray.800' colorScheme='linkedin' color='white'>
-                    <TabList>
-                        <Tab isDisabled={!areSingularsValid}>Quirky Answer</Tab>
-                        <Tab isDisabled={!areSingularsValid}>Serious Answer</Tab>
-                        <Tab isDisabled={!isCombinedValid}>Combined Answer</Tab>
-                    </TabList>
-
-                    <TabPanels>
-
-                        {
-                            (Object.keys(answers)).map(key => {
-                                return <AnswerPanel type={key} answers={answers} setAnswers={setAnswers} />
-                            })
-                        }
-
-                    </TabPanels>
-                </Tabs>
-
-            </Box>
-        </FormControl>
-    </form>
+    <>
+        <div className='w-full flex flex-col md:flex-row justify-center items-center gap-y-4 md:gap-y-0 md:gap-x-4'>
+            {
+                data.map((dataObj:Data) => {
+                    return (<Box className=' bg-fuchsia-800 shadow-inner shadow-fuchsia-900 text-white w-80 aspect-square flex rounded-lg justify-center items-center' key={dataObj.slug}>
+                        <Text>
+                            {dataObj.question}
+                        </Text>
+                    </Box>)
+                })
+            
+            }
+        </div>
+    
+    </>
   )
 }
 
 export const getServerSideProps : GetServerSideProps = async() => {
 
     const res = await fetch('http://localhost:3000/api/hello');
-    const data = await res.json()
-
+    const data:Answers[] = await res.json()
+    console.log(data);
     return {
         props: {
             data
