ad1c5a0a70004966fccc7b264aebeb5027c83b4f
reysgitcode <127261720+reysgitcode@users.noreply.github.com>
2023-03-22 16:23:49 +0800

    Create user schema

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    display_picture: String,
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+UserSchema.methods.generateJWTToken = function() {
+    const user = {
+        email: this.email,
+        id: this._id
+    }
+}
+
+UserSchema.virtual('fullname').get(function() {
+    return `${this.first_name} ${this.last_name}`
+})
+
+module.exports = mongoose.model('User', UserSchema);
