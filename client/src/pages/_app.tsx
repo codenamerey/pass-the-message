e7009add9fedfdd234c36027e9c4397ea777db46
reysgitcode <127261720+reysgitcode@users.noreply.github.com>
2023-03-26 16:38:11 +0800

    Make 'user' from server globally available

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,15 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import UserContext from '@/context/UserContext';
 import Header from '@/components/Header';
+import { useState } from 'react';
+import { User } from '@/interfaces/types.js';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [user, setUser] = useState<User | null>();
+
   return (
-    <ChakraProvider>
-      <main className=' flex flex-col bg-gradient-to-br from-fuchsia-800 to-fuchsia-400 min-h-screen'>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-    </ChakraProvider>
+    <UserContext.Provider value={{...user, setUser}}>
+      <ChakraProvider>
+        <main className=' flex flex-col bg-gradient-to-br from-fuchsia-800 to-fuchsia-400 min-h-screen'>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+      </ChakraProvider>
+    </UserContext.Provider>
   )
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,19 +6,47 @@ import {
   Button,
   Icon
 } from '@chakra-ui/react';
-import { ChangeEvent, FormEvent, useRef, useState } from 'react';
+import { useEffect, useRef, useContext } from 'react';
+import UserContext from '@/context/UserContext';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { fullname, setUser } = useContext(UserContext);
+  const fetchUser = useRef(() => {
+
+  });
+
+  fetchUser.current = async() => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/me`);
+    const user = await res.json();
+    return user
+  }
+
+  useEffect(() => {
+    if( fullname ) return
+
+    (async() => {
+      const user = await fetchUser.current();
+      setUser(user);
+
+    })();
+  }, []);
 
   return (
     <>
       <section className=' flex flex-col justify-center items-center grow text-center gap-y-4'>
-        <Heading color='whiteAlpha.600'>Relay your message to someone.</Heading>
-        <Button colorScheme='whatsapp'>Register</Button>
-        <Button colorScheme='messenger'>Register with Facebook</Button>
-        <Button colorScheme='linkedin'>Register with Google</Button>
+      {!fullname &&
+        <>
+          <Heading color='whiteAlpha.600'>Relay your message to someone.</Heading>
+          <Button colorScheme='whatsapp'>Register</Button>
+          <Button colorScheme='messenger'>Register with Facebook</Button>
+          <Button colorScheme='linkedin'>Register with Google</Button>
+        </>  
+          }
+
+      {fullname &&
+        <Heading>Welcome, {fullname}!</Heading>}
       </section>
     </>
   )
