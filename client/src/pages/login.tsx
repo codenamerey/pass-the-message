6bb89bdf6b34ecf919c0a49baeb05a686efcbcf7
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-06-27 17:50:40 +0800

    Build log in page

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,27 @@
+import Form from '@/components/Form'
+import { Button, FormLabel, Input } from '@chakra-ui/react'
+import React from 'react'
+
+const login = () => {
+  return (
+    <>
+        <Form>
+            <FormLabel>
+                Username/Email Address:
+            </FormLabel>
+            <Input type="email" />
+
+            <FormLabel>
+                Password
+            </FormLabel>
+            <Input type="password" />
+
+            <Button colorScheme="whatsapp">
+                Log In
+            </Button>
+        </Form>
+    </>
+  )
+}
+
+export default login
